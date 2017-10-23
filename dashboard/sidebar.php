6e13909d25e6f01137cbb64eb90ad6ad2e177f5b
Bols <lenovobolante@gmail.com>
2017-10-24 04:34:59 +0800

    added sidebar

---
+++ dashboard/sidebar.php
@@ -0,0 +1,11 @@
+<div class="middle-header">
+        <div class="logo">
+             
+            <img src="images/logo.png" >     
+        </div>  
+                <ul>
+                    <li class="current"><a href="#"><i class="fa fa-bar-chart"></i> Google Marketing</a></li>
+                    <li><a href="my_subscriptions.php"><i class="fa fa-check-square-o"></i> My Subscriptions</a></li>
+                    <li><a href="my_payment.php"><i class="fa fa-code"></i> Development Service</a></li>
+                </ul>
+    </div>
